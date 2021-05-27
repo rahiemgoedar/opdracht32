b34d3ebb252cf7fb625a2e3e5f238199693491ce
rahiemgoedar <79698303+rahiemgoedar@users.noreply.github.com>
2021-05-27 17:28:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+//deel 1
+const button = document.querySelector('#mybutton');
+
+button.addEventListener('click', function(e){
+    console.log(e.target);
+    console.log(e);
+    alert('button clicked');
+});
+
+//deel 2
+const button2 = document.querySelector("#changebackground");
+
+button2.addEventListener('click', function(){
+    
+});
